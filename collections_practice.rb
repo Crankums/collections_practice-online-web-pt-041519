88b0452b2ec105be0d1b857060aca1546084d80c
Craig Donavin <crdonavin@gmail.com>
2019-04-17 14:08:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array)
+  array.sort do | a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort {|a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  swap_a = array[1]
+  swap_b = array[2]
+  array[1] = swap_b
+  array[2] = swap_a
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |string|
+      string[2] = "$"
+    end
+end
+
+def find_a(array)
+  array.select { |string| string.start_with?("a") }
+end
+
+def sum_array(array)
+  # sum = 0
+  # array.each {|element| sum += element}
+  # sum
+  array.sum
+end
+
+def add_s(array)
+new_arr = []
+  array.each do |string|
+    new_str = string + "s"
+    new_arr << new_str
+  end
+  new_arr
+end
